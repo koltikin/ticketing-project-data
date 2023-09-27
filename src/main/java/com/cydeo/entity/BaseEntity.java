bf88614be47e6b9957fcfef33f5564fb7cf760a0
ziya <koltikin210@gmail.com>
2023-09-27 14:23:18 +0300

    dependency added, database connected, baseEntity created

---
+++ src/main/java/com/cydeo/entity/BaseEntity.java
@@ -0,0 +1,21 @@
+package com.cydeo.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@MappedSuperclass
+public class BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private LocalDateTime insertDateTime;
+    private Long insertUserId;
+    private LocalDateTime lastUpdateDateTime;
+    private long lastUpdateUserId;
+}
