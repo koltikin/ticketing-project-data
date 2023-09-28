c93fcb41cc26915213381953922c05ac4148f402
ziya <koltikin210@gmail.com>
2023-09-28 20:19:43 +0300

    user delete function updated

--- src/main/java/com/cydeo/Repository/UserRepository.java
+++ src/main/java/com/cydeo/Repository/UserRepository.java
@@ -3,7 +3,6 @@ package com.cydeo.Repository;
 import com.cydeo.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
 import javax.transaction.Transactional;
 
 @Repository
